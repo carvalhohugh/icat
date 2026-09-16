30ae5660a01bfe3a5d3f5d0afd4e847fadfda732
Hugo Carvalho <hugocamposdecarvalho@gmail.com>
2026-09-16 18:21:56 -0300

    fix: swap broken next-pwa library with @ducanh2912/next-pwa fork to fix App Router navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from 'next-pwa';
+import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
 });
 
 const nextConfig = {
